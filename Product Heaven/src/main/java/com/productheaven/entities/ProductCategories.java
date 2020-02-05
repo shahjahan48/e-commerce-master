8d707754b8c3d60115699e2303fc4c2c623499c6
imran110219 <imran110219@gmail.com>
2020-02-05 21:24:06 +0600

    persistence api

---
+++ Product Heaven/src/main/java/com/productheaven/entities/ProductCategories.java
@@ -0,0 +1,66 @@
+package com.productheaven.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Imran on 2/5/2020.
+ */
+@Entity
+@Table(name = "product_categories", schema = "product_heaven", catalog = "")
+public class ProductCategories {
+    private long id;
+    private String name;
+    private String description;
+
+    @Id
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductCategories that = (ProductCategories) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
